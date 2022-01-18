a3d40f93df78c223c1309e57208421775d1fedab
janginbeom <ibjang@humaxparcs.com>
2022-01-18 14:28:24 +0900

    fix

--- kafka-monitor/exec-kafka-monitor.sh
+++ kafka-monitor/exec-kafka-monitor.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-docker run -d --name kafka-monitor-test-server -p 15051:15050 \
+docker run -d --name kafka-monitor-local -p 15052:8080 \
 	-e KAFKA_CLUSTERS_0_NAME=kafka-broker1 \
-	-e KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS=61.97.185.112:9092 \
-	-e KAFKA_CLUSTERS_0_ZOOKEEPER=61.97.185.112:2181,61.97.185.112:2182,61.97.185.112:2183 \
+	-e KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS=192.168.30.101:9092 \
+	-e KAFKA_CLUSTERS_0_ZOOKEEPER=zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 \
 	-e KAFKA_CLUSTERS_1_NAME=kafka-broker2 \
-	-e KAFKA_CLUSTERS_1_BOOTSTRAPSERVERS=61.97.185.112:9093 \
-	-e KAFKA_CLUSTERS_1_ZOOKEEPER=61.97.185.112:2181,61.97.185.112:2182,61.97.185.112:2183 \
+	-e KAFKA_CLUSTERS_1_BOOTSTRAPSERVERS=192.168.30.102:9092 \
+	-e KAFKA_CLUSTERS_1_ZOOKEEPER=zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 \
 	-e KAFKA_CLUSTERS_2_NAME=kafka-broker3 \
-	-e KAFKA_CLUSTERS_2_BOOTSTRAPSERVERS=61.97.185.112:9094 \
-	-e KAFKA_CLUSTERS_2_ZOOKEEPER=61.97.185.112:2181,61.97.185.112:2182,61.97.185.112:2183 \
-	-e SERVER_PORT=15050 \
+	-e KAFKA_CLUSTERS_2_BOOTSTRAPSERVERS=192.168.30.103:9092 \
+	-e KAFKA_CLUSTERS_2_ZOOKEEPER=zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 \
 	provectuslabs/kafka-ui:latest
 
-# docker network connect zookeeper_default kafka-monitor-local
-# docker network connect kafka_default kafka-monitor-local
+docker network connect zookeeper_default kafka-monitor-local
+docker network connect kafka_default kafka-monitor-local
 
 exit 0
