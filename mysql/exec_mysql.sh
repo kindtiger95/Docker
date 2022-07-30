aa3a9cb0329ac42f150fcfe3cca1052699437b3d
janginbeom <jjanginbeom@naver.com>
2022-07-31 03:01:54 +0900

    mysql 한글 설정

--- mysql/exec_mysql.sh
+++ mysql/exec_mysql.sh
@@ -4,4 +4,6 @@ docker run -d --name mysql \
 	-e MYSQL_ROOT_PASSWORD=ibjang123 \
 	-p 23306:3306 \
 	-v ~/data/mysql:/var/lib/mysql \
-	mysql:5.7.38
+	mysql:5.7.38 \
+	--character-set-server=utf8 \
+	--collation-server=utf8_general_ci
