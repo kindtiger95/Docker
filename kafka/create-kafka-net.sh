a72ecad64674661fa5874dc88993c89b46704eb0
janginbeom <jjanginbeom@naver.com>
2022-10-16 17:02:04 +0900

    kafka file 수정

--- kafka/create-kafka-net.sh
+++ kafka/create-kafka-net.sh
@@ -2,7 +2,7 @@
 
 docker network create \
 --driver=bridge \
---subnet=192.168.30.0/24 \
---ip-range=192.168.30.0/24 \
---gateway=192.168.30.1 \
+--subnet=192.168.40.0/24 \
+--ip-range=192.168.40.0/24 \
+--gateway=192.168.40.1 \
 kafka-net
